287dc00e2638a9b9c07eac20ba38d8c4a558a655
Krit Kruaykitanon <kritkruaykitanon@gmail.com>
2021-07-18 22:17:15 +0700

    fix end point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { get } from "lodash";
 import bodyParser from "body-parser";
 import "dotenv/config";
 import express from "express";
@@ -5,13 +6,14 @@ import moment from "moment-timezone";
 import { lineClient } from "./lib/line-client";
 import { foodService } from "./services/food.service";
 import { messageService } from "./services/message.service";
-
-export const app = express();
-app.use(bodyParser.json());
-const port = process.env.PORT || 8080;
+import { COMMAND } from "./types/enums";
 
 moment.locale("th");
 moment.tz.setDefault("Asia/Bangkok");
+
+const app = express();
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
@@ -31,10 +33,36 @@ app.post("/what-to-eat", async (req, res) => {
 
 app.get("/food", async (req, res) => {
   const excludeRecent = (req.query?.excludeRecent as string) === "true";
-
   const foods = await foodService.findAll({ excludeRecent });
-
   return res.send({ length: foods.map((food) => food._rawJson) });
+});
+
+app.post("/webhook", async (req, res) => {
+  const event = get(req, ["body", "events", "0"]);
+  const message = get(event, ["message", "text"]);
+  const replyToken = get(event, "replyToken") as string;
+
+  try {
+    switch (message) {
+      case COMMAND.WHAT_TO_EAT: {
+        const menu = await foodService.getRandomMenu();
+        const message = messageService.buildFoodMenuMsg(menu);
+        await lineClient.replyMessage(replyToken, message);
+      }
+
+      case COMMAND.REROLL: {
+        const menu = await foodService.getRerolledRandomMenu();
+        const message = messageService.buildFoodMenuMsg(menu);
+        await lineClient.replyMessage(replyToken, message);
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    await lineClient.broadcast({ type: "text", text: JSON.stringify(e) });
+    return res.sendStatus(400).send(e);
+  }
+
+  return res.sendStatus(200);
 });
 
 app.listen(port, () => {
